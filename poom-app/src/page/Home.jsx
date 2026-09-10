81318b09e57932f69ec6e43e52bdbb224945d7ba
Nutthap00m <neungnet_n@silpakorn.edu>
2026-09-10 04:48:11 +0000

    Like

--- poom-app/src/page/Home.jsx
+++ poom-app/src/page/Home.jsx
@@ -1,8 +1,17 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import { movies } from "../data/data";
+import MovieCard from "../component/MovieCard";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+
+      <div className="mx-auto mt-8 max-w-5xl">
+        <MovieCard title="InThe Mandalorian and Grogu" year={2026}/>
+        </div>
     </div>
   );
 }
